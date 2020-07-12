88f41a359ebcacfd71783e58f6cb467c5395d73e
Maria Petrova <Maria Petrova>
2020-07-12 17:15:45 +0300

    Updates

---
+++ precache-manifest.894f4a7698eb1c180332e106a9faa977.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29433f35fc39857a9946b938eddfbc0f",
+    "url": "/JM21/index.html"
+  },
+  {
+    "revision": "8268af93128ce516619c",
+    "url": "/JM21/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "458151c21c12262df51f",
+    "url": "/JM21/static/css/main.a70b6f4d.chunk.css"
+  },
+  {
+    "revision": "8268af93128ce516619c",
+    "url": "/JM21/static/js/2.3891737d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/JM21/static/js/2.3891737d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "458151c21c12262df51f",
+    "url": "/JM21/static/js/main.4e33ab13.chunk.js"
+  },
+  {
+    "revision": "649a614ba9c43019fa9b",
+    "url": "/JM21/static/js/runtime-main.5cadde27.js"
+  },
+  {
+    "revision": "7a54497bc51fed1159f51131c36366d2",
+    "url": "/JM21/static/media/Inter-Bold.7a54497b.ttf"
+  },
+  {
+    "revision": "9aa03c46fbe41e25c51289a3e78e9454",
+    "url": "/JM21/static/media/Inter-Bold.9aa03c46.woff2"
+  },
+  {
+    "revision": "a956603bbbedf420d94018de31664aaa",
+    "url": "/JM21/static/media/Inter-Bold.a956603b.woff"
+  },
+  {
+    "revision": "c135b2695edf08af6477b8dfcb7922a5",
+    "url": "/JM21/static/media/Inter-Bold.c135b269.eot"
+  },
+  {
+    "revision": "24a88db8dcb2ace5ed6efdc866dd03f5",
+    "url": "/JM21/static/media/Inter-Regular.24a88db8.woff2"
+  },
+  {
+    "revision": "59e981f7c001d676710ae161ce5d8258",
+    "url": "/JM21/static/media/Inter-Regular.59e981f7.ttf"
+  },
+  {
+    "revision": "a51fb83eeb728f495a462678021f09b3",
+    "url": "/JM21/static/media/Inter-Regular.a51fb83e.woff"
+  },
+  {
+    "revision": "d03619e141b22d7eae67d9a7c814424b",
+    "url": "/JM21/static/media/Inter-Regular.d03619e1.eot"
+  }
+]);
